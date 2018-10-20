71109e667f5d3ca67c053559d093cc064d3c56b7
v1ll4n <v1ll4n@villanch.top>
2018-10-21 00:48:13 +0800

    [feature] auto update

--- schedium/core.py
+++ schedium/core.py
@@ -3,7 +3,7 @@
 import uuid
 import typing
 from datetime import datetime, timedelta
-from threading import Timer, Thread
+from threading import Timer, Thread, Event
 import time
 
 from .handlers import DefaultTaskHandler
@@ -12,7 +12,7 @@ from .handlers import DefaultTaskHandler
 class _CurrentTask(Timer):
 
     def __init__(self, id: str, next_execute_time: datetime, target: typing.Callable, vargs: tuple = (),
-                 kwargs: typing.Optional[typing.Mapping[str, typing.Any]] = None):
+                 kwargs: typing.Optional[typing.Mapping] = None):
         self.next_execute_time: datetime = next_execute_time
         interval = max(next_execute_time.timestamp() - time.time(), 0)
         self.id = id
@@ -23,11 +23,42 @@ class _CurrentTask(Timer):
 
 class Schedium(object):
 
-    def __init__(self, task_handler=None):
+    def __init__(self, update_interval=30, task_handler=None):
         self._task_handler = task_handler or DefaultTaskHandler()
         self._current_task: typing.Optional[_CurrentTask] = None
 
+        self._auto_update_thread: Thread = None
+        self._auto_update_event = Event()
+
         self.update()
+
+    def start_auto_update(self, empty_update_interval=5, normal_auto_update=30):
+        thr = Thread(target=self._auto_update, kwargs={
+            "empty_update_interval": empty_update_interval,
+            "normal_auto_update": normal_auto_update
+        })
+        thr.daemon = True
+        self._auto_update_thread = thr
+        thr.start()
+
+    def join(self, timeout=None):
+        if not self._auto_update_thread:
+            self.start_auto_update()
+
+        self._auto_update_thread.join(timeout=timeout)
+
+    def stop(self):
+        self._auto_update_event.clear()
+        self._auto_update_thread = None
+
+    def _auto_update(self, empty_update_interval, normal_auto_update):
+        self._auto_update_event.set()
+        while self._auto_update_event.is_set():
+            self.update()
+            if self._current_task:
+                time.sleep(normal_auto_update)
+            else:
+                time.sleep(empty_update_interval)
 
     def execute_later(self, after: typing.Union[int, float], target: typing.Callable, vargs: tuple = (),
                       kwargs: typing.Optional[typing.Mapping[str, str]] = None, id: str = None):
@@ -48,8 +79,8 @@ class Schedium(object):
     def execute_interval(self, interval: typing.Union[int, float], target: typing.Callable,
                          vargs: typing.Optional[tuple] = None,
                          kwargs: typing.Optional[typing.Mapping[str, str]] = None,
-                         first: bool=True, start: typing.Optional[datetime]=None,
-                         end: typing.Optional[datetime]=None, id=None):
+                         first: bool = True, start: typing.Optional[datetime] = None,
+                         end: typing.Optional[datetime] = None, id=None):
         id = id or uuid.uuid4().hex
 
         if all([start, end]):
